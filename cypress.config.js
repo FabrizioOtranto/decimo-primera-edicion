5eeb699b30ae12709b4b0098f50fe98dba58bacb
Fabrizio Otranto <Fabryotranto@gmail.com>
2025-05-07 18:17:54 -0300

    config project

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    watchForFileChanges: false,
+    defaultCommandTimeout: 3000,
+    baseUrl: 'https://pushing-it.vercel.app/',
   },
 });
